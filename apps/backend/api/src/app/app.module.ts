146952badf39d981820649cdc0696a5e7e0ca4c1
Deveesh Shetty <deveeshshetty@gmail.com>
2024-11-01 23:35:19 +0530

    feat: connects postgres db

--- apps/backend/api/src/app/app.module.ts
+++ apps/backend/api/src/app/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { databaseConfig } from 'apps/backend/api/src/config/database.config';
+import { HealthController } from './health/health.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot(databaseConfig),
+  ],
+  controllers: [AppController, HealthController],
   providers: [AppService],
 })
 export class AppModule {}
