55e87c733015e4ff98c1e6962a3d571435fb4a53
Rohan <rohankumar5803@gmail.com>
2024-12-24 16:46:23 +0530

    update api endpoints

--- apps/backend/api/src/config/database.config.ts
+++ apps/backend/api/src/config/database.config.ts
@@ -4,9 +4,9 @@ export const databaseConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  username: 'rohan',
+  password: 'rohan',
+  database: 'bus_tracking_db',
   entities: ['dist/**/*.entity{.ts,.js}'],
   synchronize: false, // Use migrations instead
 };
